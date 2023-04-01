04353c21d4e2030bcbff56748f35e10bd9460988
kimeshan <kimeshan@gmail.com>
2023-04-02 00:27:19 +0100

    Test Food Controller

--- src/food/dto/create-food.dto.ts
+++ src/food/dto/create-food.dto.ts
@@ -1,1 +1,31 @@
-export class CreateFoodDto {}
+// food-created.dto.ts
+
+import { FoodSource } from '@prisma/client';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateFoodDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(1)
+  @MinLength(255)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  numberOfGrams: number;
+
+  @IsNotEmpty()
+  @IsEnum(FoodSource)
+  source: FoodSource;
+}
